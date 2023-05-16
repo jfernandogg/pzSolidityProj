c7c1d4fedc1117f2094bffbdf7c161509d279f2e
Juan Fernando Gallego Gomez <juan.gallego@gmail.com>
2023-05-16 15:36:24 -0500

    Update ensuranceContract.sol
    Added functionality to create a new policy

--- ensuranceContract.sol
+++ ensuranceContract.sol
@@ -3,10 +3,75 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract insurancePolicies {
+  
+
+
+    struct Tpoliza {
+        string id;
+        address payable ensured;
+        string idbenefitiary;
+        TEstates state;
+        uint rates;
+    }
+    struct Trates {
+      mapping(string => uint) rate;
+    }
+
+  // States of the policy
+  //    Active policy means the risk is protected
+  //    Retired means the customer is retired and policy is no longer valid
+  //    Loss means the risk was materialized and a claim was made
   enum TEstates { Active, Retired, Loss }
+  // TEProduct are the tipes of policies available.
   enum TEProduct { life, health, hospital, elder }
-  mapping tarifas (string => uint);
-  struct poliza {
-    string id;
+
+  // Container for all policies
+  Tpoliza[] public polizas;
+  Trates rates;
+
+constructor() {
+  rates.rate["life"] = 100;
+  rates.rate["health"] = 200;
+  rates.rate["hospital"] = 300;
+  rates.rate["elder"] = 400;
+}
+
+function addPolicy(string memory _id, address payable _ensured, string memory _idbenefitiary, TEstates _state, TEProduct _product, uint _rate) public {
+        require(_rate > 0, "Rate must be greater than zero.");
+        // verify restrictions according to TEProduct
+        if (_product == TEProduct.life) {
+            require(_rate <= rates.rate["life"], "Rate exceeds maximum limit for ProductA.");
+        } else if (_product == TEProduct.health) {
+            require(_rate <= rates.rate["health"], "Rate exceeds maximum limit for ProductB.");
+        } else if (_product == TEProduct.hospital) {
+            require(_rate <= rates.rate["hospital"], "Rate exceeds maximum limit for ProductC.");
+        } else if (_product == TEProduct.elder) {
+            require(_rate <= rates.rate["elder"], "Rate exceeds maximum limit for ProductC.");
+        }
+        Tpoliza memory newPolicy = Tpoliza(_id, _ensured, _idbenefitiary, _state, _rate);
+        polizas.push(newPolicy);
   }
+
+
+  function RetirePolicy(string memory _id) public {
+
+  }
+
+  function claimPolicy(string memory _id) public {
+  
+  }
+
+  function totalAmountActivePolicy() public {
+
+  }
+
+  function totalAmountRetiredPolicy() public {
+
+  }
+
+  function totalAmountClaimed() public {
+
+  }
+
+
 }
